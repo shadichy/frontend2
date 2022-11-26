0919cdf404335044b06d4aa9cb9521780b4052d8
shadichy <hotrobb360web@gmail.com>
2022-11-26 21:53:07 +0700

    init stgaes

---
+++ src/defaults.ts
@@ -0,0 +1,17 @@
+export interface Required {
+  lang?: string,
+  filename: string,
+  meta_title?: string,
+  meta_desc: string,
+  meta_img?: string,
+  nav_home: string,
+  nav_info: string,
+  nav_gall: string,
+  nav_news: string,
+  nav_comm: string,
+  nav_down: string,
+  nav_join: string,
+  nav_dl: string,
+  credit: string,
+  links: string
+}
