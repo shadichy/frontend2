fc57c482acb354e58a21a5c13c85ef81c0e41cf5
shadichy <hotrobb360web@gmail.com>
2022-11-27 19:52:27 +0700

    lang now works

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,17 +1,5 @@
-export interface Required {
-  lang?: string,
-  filename: string,
+export interface MetaRequired {
   meta_title?: string,
   meta_desc: string,
-  meta_img?: string,
-  nav_home: string,
-  nav_info: string,
-  nav_gall: string,
-  nav_news: string,
-  nav_comm: string,
-  nav_down: string,
-  nav_join: string,
-  nav_dl: string,
-  credit: string,
-  links: string
+  meta_img?: string
 }
